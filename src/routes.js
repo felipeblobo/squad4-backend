9b3c4a18e7310bec85261eb2922499fafdd31ae6
Felipe Lobo <felipeblobo@gmail.com>
2021-09-02 14:46:37 -0300

    configurações iniciais. instalando mysql2

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  res.send('Agendamento')
+  res.status(200)
+  res.send({ app: "agendamento"})
 })
 
 module.exports = routes;
