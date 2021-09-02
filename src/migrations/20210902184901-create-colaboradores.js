497ecadec22e1d65f5f5e8dbb5114072d638c3d5
Felipe Lobo <felipeblobo@gmail.com>
2021-09-02 16:07:25 -0300

    conectando ao db e criando model de colaboradores

---
+++ src/migrations/20210902184901-create-colaboradores.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Colaboradores', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      origin_office: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      vaccine_status: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Colaboradores');
+  }
+};
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const server = express();
 const port = 8080;
 
-server.use(express.urlencoded());
+server.use(express.urlencoded({ extended: true}))
 server.use(express.json());
 server.use(routes);
 
