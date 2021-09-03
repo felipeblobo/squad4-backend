7069d2ce138958da8f2ac4fd66a942e93823b8de
Felipe Lobo <felipeblobo@gmail.com>
2021-09-02 23:21:06 -0300

    rota de selecionar colaborador por id

--- src/controllers/ColaboradorController.js
+++ src/controllers/ColaboradorController.js
@@ -7,8 +7,18 @@ class ColaboradorController {
         const todosOsColaboradores = await database.Colaboradores.findAll();
         return res.status(200).json(todosOsColaboradores);
       } catch (error) {
-        return res.status(500).json(error.message)
+        return res.status(500).json(error.message);
       }      
+    }
+
+    static async colaboradorPorId(req, res) {
+      const { id } = req.params;
+      try {
+        const colaborador = await database.Colaboradores.findOne({ where: { id: Number(id)}});
+        return res.status(200).json(colaborador);
+      } catch (error) {
+        return res.status(500).json(error.message);
+      }
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const routes = express.Router();
 const ColaboradorController = require('./controllers/ColaboradorController');
 
-routes.get('/', (req, res) => {
-  res.status(200)
-  res.send({ app: "agendamento"})
-})
+const routes = express.Router();
 
-routes.get('/colaboradores', ColaboradorController.listarColaboradores)
+routes.get('/colaboradores', ColaboradorController.listarColaboradores);
+routes.get('/colaboradores/:id', ColaboradorController.colaboradorPorId);
 
 module.exports = routes;
