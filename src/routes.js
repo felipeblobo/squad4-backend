52089b4651d37acd129afc9c1f94cd1b53d23e0e
Felipe Lobo <felipeblobo@gmail.com>
2021-09-07 09:45:45 -0300

    tornando rotas acessiveis somente autenticadas

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const UserController = require("./controllers/UserController");
 const SchedulingController = require("./controllers/SchedulingController");
+const login = require('./middlewares/login');
 
 const routes = express.Router();
 
-routes.get("/colaboradores", UserController.listUsers);
-routes.get("/colaboradores/:id", UserController.userById);
+routes.get("/colaboradores", login, UserController.listUsers);
+routes.get("/colaboradores/:id", login, UserController.userById);
 routes.post("/colaboradores", UserController.userRegistration);
 routes.post("/login", UserController.login);
 
-routes.get("/agendamentos", SchedulingController.listScheduling);
+routes.get("/agendamentos", login, SchedulingController.listScheduling);
 routes.get(
-  "/agendamentos/colaborador/:id",
+  "/agendamentos/colaborador/:id", login,
   SchedulingController.listSchedulingByUserId,
 );
-routes.get("/agendamentos/data", SchedulingController.schedulingByDate);
-routes.post("/agendamentos", SchedulingController.registerScheduling);
-routes.put("/agendamentos/:id", SchedulingController.updateScheduling);
-routes.delete("/agendamentos/:id", SchedulingController.deleteScheduling);
+routes.get("/agendamentos/data", login, SchedulingController.schedulingByDate);
+routes.post("/agendamentos", login, SchedulingController.registerScheduling);
+routes.put("/agendamentos/:id", login, SchedulingController.updateScheduling);
+routes.delete("/agendamentos/:id", login, SchedulingController.deleteScheduling);
 
 module.exports = routes;
