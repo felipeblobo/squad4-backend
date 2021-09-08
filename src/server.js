b0b07ab00e7fdd82836a6d0952fc23e14ba85e76
dansenpir <senegalha@protonmail.com>
2021-09-08 20:48:55 -0300

    Ajustes na funcionalidade de login

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ const routes = require("./routes");
 const server = express();
 const port = 8080;
 
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
-server.use(routes);
 server.use(cors({
   origin: '*'
 }));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(routes);
 
 server.listen(port, console.log(`Server running on port ${port}.`));
