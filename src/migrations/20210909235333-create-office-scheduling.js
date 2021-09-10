2efc5dea9d8e7926953b8ef54f8c9f6b20865f19
Felipe Lobo <felipeblobo@gmail.com>
2021-09-09 21:21:15 -0300

    adequando db para refletir necessidades do projeto

--- src/migrations/20210909235333-create-office-scheduling.js
+++ src/migrations/20210909235333-create-office-scheduling.js
@@ -28,6 +28,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      time_zone:  {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- src/models/officescheduling.js
+++ src/models/officescheduling.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
   };
   OfficeScheduling.init({
     date: DataTypes.DATEONLY,
+    time_zone: DataTypes.INTEGER,
     room: DataTypes.STRING,
     office: DataTypes.STRING
   }, {
