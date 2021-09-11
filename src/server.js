4eef43538cfbd099b0b69d50973352cecaef30ff
Felipe Lobo <felipeblobo@gmail.com>
2021-09-11 19:00:38 -0300

    adequando cors

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 8080;
 server.use(cors({
   origin: '*'
 }));
+server.options('*', cors());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(routes);
