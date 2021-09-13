65e51d1dce16a99267965960c9a9c7705c2eea05
Felipe Lobo <felipeblobo@gmail.com>
2021-09-13 17:19:20 -0300

    ajustando cors

--- src/server.js
+++ src/server.js
@@ -6,14 +6,15 @@ const port = process.env.PORT || 8080;
 const swaggerFile=require('./swagger/swagger_output.json');
 const swaggerUi = require("swagger-ui-express");
 
-server.options("/", cors(), function (req, res) {
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.end();
-});
-server.use(cors({
-  origin: "*"
-}));
+cors({credentials: true, origin: true});
+server.use(cors());
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Tupe, Accept, x-access-token");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+})
+
 server.use('/docs',swaggerUi.serve, swaggerUi.setup(swaggerFile));
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
