943fe6277541bd83d8b8a837bc0b4740d2d6d6ed
Felipe Lobo <felipeblobo@gmail.com>
2021-09-13 17:23:34 -0300

    ajustando cors

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ const UserController = require("./controllers/UserController");
 const SchedulingController = require("./controllers/SchedulingController");
 const login = require('./middlewares/login');
 const RoomSchedulingController = require("./controllers/RoomSchedulingController");
-const cors = require('cors');
 
 const routes = express.Router();
 
@@ -11,7 +10,7 @@ routes.get("/api/v1/colaboradores", login, UserController.listUsers);
 routes.get("/api/v1/colaboradores/:id", login, UserController.userById);
 routes.post("/api/v1/colaboradores", UserController.userRegistration);
 routes.put("/api/v1/login/firstaccess/:id", UserController.updateRegister);
-routes.post("/api/v1/login", cors(), UserController.login);
+routes.post("/api/v1/login", UserController.login);
 routes.get("/api/v1/colaboradores/verificacao/:token", UserController.modifyUserWithVerifiedEmail);
 
 routes.get("/api/v1/agendamentos", SchedulingController.listScheduling);
