343eb4f74e53a5fe9d720b7244e848cf052e0561
Felipe Lobo <felipeblobo@gmail.com>
2021-09-13 08:00:03 -0300

    adicionando documentação swagger

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const cors = require("cors")
 const routes = require("./routes");
 const server = express();
 const port = process.env.PORT || 8080;
+const swaggerFile=require('./swagger/swagger_output.json');
+const swaggerUi = require("swagger-ui-express");
 
+server.use('/',swaggerUi.serve, swaggerUi.setup(swaggerFile));
 server.options("/", cors(), function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://orange-space.herokuapp.com/");
   res.setHeader("Access-Control-Allow-Methods", "*");
